36b741c3740f3e7c369600ac62bb72917737d886
Саша Филиппов <apple@MacBook-Pro.local>
2022-09-02 21:46:15 +0300

    removed drivers folder

--- conftest.py
+++ conftest.py
@@ -11,25 +11,28 @@ def pytest_addoption(parser):
     parser.addoption(
         "--headless", action="store_true", help="browser to run test"
     )
-
+    parser.addoption(
+        "--drivers", default=os.path.expanduser("~/Documents/otus/drivers")
+    )
 
 @pytest.fixture()
 def driver(request):
     browser_name = request.config.getoption("--browser")
-    headless = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
+    drivers_source = request.config.getoption("--drivers")
 
     if browser_name == "chrome":
         options = webdriver.ChromeOptions()
         if headless:
             options.headless = True
         _driver = webdriver.Chrome(
-            executable_path=os.path.expanduser("~/Documents/otus/drivers/chromedriver"),
+            executable_path=os.path.expanduser(f"{drivers_source}/chromedriver"),
             options=options
         )
     elif browser_name == "firefox":
-        _driver = webdriver.Firefox(executable_path=os.path.expanduser("~/Documents/otus/drivers/geckodriver"))
+        _driver = webdriver.Firefox(executable_path=os.path.expanduser(f"{drivers_source}/geckodriver"))
     elif browser_name == "yandex":
-        _driver = webdriver.Chrome(executable_path=os.path.expanduser("~/Documents/otus/drivers/yandexdriver"))
+        _driver = webdriver.Chrome(executable_path=os.path.expanduser(f"{drivers_source}/yandexdriver"))
     else:
         raise ValueError(f"Browser {browser_name} is not support")
 
